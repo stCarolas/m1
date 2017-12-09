9e9b94b981fdc0c7a774f3a023066ddb2513e949
stCarolas <stcarolas@gmail.com>
2017-12-10 01:40:08 +0300

    add startup script for fzf

--- m1.go
+++ m1.go
@@ -12,12 +12,7 @@ const  DEFAULT_CONFIG_PATH string = ".m1"
 
 type menu struct {
   configPath string
-  actions    []action
-}
-
-type action struct {
-  name    string
-  command string
+  actions    map[string]string
 }
 
 func (menu *menu) loadMenu() {
@@ -30,13 +25,11 @@ func (menu *menu) loadMenu() {
   }
   defer config.Close()
 
-  menu.actions = make([]action, 0)
+  menu.actions = make(map[string]string)
   scanner := bufio.NewScanner(config)
   for scanner.Scan() {
       line := strings.Split(scanner.Text(),":")
-      menu.actions = append(menu.actions, action{
-        name:    line[0],
-        command: line[1] })
+      menu.actions[line[0]] = line[1]
   }
 
   if err := scanner.Err(); err != nil {
@@ -47,17 +40,12 @@ func (menu *menu) loadMenu() {
 func main() {
   menu := menu{configPath:DEFAULT_CONFIG_PATH}
   menu.loadMenu()
-  switch os.Args[1] {
-  case "actions":
-    for _, action := range menu.actions {
-      fmt.Println(action.name)
+  if len(os.Args) == 1 {
+    for name := range menu.actions {
+      fmt.Println(name)
     }
-  case "run":
-    target := os.Args[2]
-    for _, action := range menu.actions {
-      if action.name == target {
-        fmt.Println(action.command)
-      }
-    }
+  } else {
+    target := os.Args[1]
+    fmt.Println(strings.TrimSpace(menu.actions[target]))
   }
 }
---
+++ start.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+#
+# start.sh
+# Copyright (C) 2017 stcarolas <stcarolas@homeGround>
+#
+# Distributed under terms of the MIT license.
+#
+
+exec $(m1 $(m1 | fzf))
